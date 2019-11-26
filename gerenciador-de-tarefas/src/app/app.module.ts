2ba5c40ec73eaf1d329e2fe3715d814015a66ce7
Darlan Talles <darlantalles@gmail.com>
2019-11-25 22:54:45 -0300

    criando o routerModule

--- gerenciador-de-tarefas/src/app/app.module.ts
+++ gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TarefasModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
